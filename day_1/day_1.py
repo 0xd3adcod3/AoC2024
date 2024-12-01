239f6e6ef91ce4c0f4fff3ac242154e00d35bea6
szkiss04 <104894648+szkiss04@users.noreply.github.com>
2024-12-01 14:32:03 +0100

    First commit, AoC2024 started.
    This is the first commit, and the solution for the first puzzle. Both parts are solved!

---
+++ day_1/day_1.py
@@ -0,0 +1,37 @@
+from typing import *
+
+def get_lists_distance(lst_a: List, lst_b: List) -> int:
+    lst_a.sort()
+    lst_b.sort()
+    distance = 0
+    
+    for id_a, id_b in zip(lst_a, lst_b):
+        distance += abs(id_a - id_b)
+    
+    return distance
+
+def get_lists_similarity_score(lst_a: List, lst_b: List) -> int:
+    score: int = 0
+    
+    for number in lst_a:
+        score += number * lst_b.count(number)
+    
+    return score
+
+def parse_numlists_from_file(path: str) -> Tuple[List, List]:
+    lst_a: List = []
+    lst_b: List = []    
+    with open(path) as file:
+        for line in file:
+            num_strs = line.split()
+            lst_a.append(int(num_strs[0]))
+            lst_b.append(int(num_strs[1]))
+    
+    return lst_a, lst_b
+
+if __name__ == "__main__":
+    num_lsts = parse_numlists_from_file("day_1_input.txt")
+    dist = get_lists_distance(num_lsts[0], num_lsts[1])
+    sim_score = get_lists_similarity_score(num_lsts[0], num_lsts[1])
+    print(f"Distance between two lists: {dist}")
+    print(f"Similarity score: {sim_score}")
